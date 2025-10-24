44f57096ce7500fb730edc56ff3776d32ca5eb0e
AnthoPerez <anthoperez2502@gmail.com>
2025-10-24 13:01:04 -0500

    Eliminar la ruta del componente Dashboard y su referencia en el menú de navegación.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,15 +5,13 @@ import { GastoFormComponent } from './components/gasto-form/gasto-form';
 import { GastoListComponent } from './components/gasto-list/gasto-list';
 import { ExcelImporterComponent } from './components/excel-importer/excel-importer';
 import { ReportGeneratorComponent } from './components/report-generator/report-generator';
-// Probablemente tendrás un componente para el Dashboard también
-// import { DashboardComponent } from './components/dashboard/dashboard.component';
+
 
 export const routes: Routes = [
   // Redirección por defecto: si entran a la raíz, llévalos a la lista de gastos
   { path: '', redirectTo: '/gastos', pathMatch: 'full' },
   
   // Define la ruta para cada sección
-  // { path: 'dashboard', component: DashboardComponent },
   { path: 'registrar', component: GastoFormComponent },
   { path: 'gastos', component: GastoListComponent },
   { path: 'importar', component: ExcelImporterComponent },
